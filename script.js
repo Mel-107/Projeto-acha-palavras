ca9e6294af483c7f304db65a1f61b3293d81198a
Mel-107 <mel.miranda@escola.pr.gov.br>
2025-07-01 13:37:05 +0000

    ajustei algumas coisas

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 const BotaoMostraPalavras = document.querySelector('#botao-palavrachave');
 
 BotaoMostraPalavras.addEventListener('click, mostraPalavrasChave')
+
+function mostraPalavrasChave() {
+    const texto = document.querySelector('#entrada-de-texto')
+}
