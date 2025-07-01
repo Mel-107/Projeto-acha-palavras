d016e53f50475bb768f9f53437410530213597ef
Mel-107 <mel.miranda@escola.pr.gov.br>
2025-07-01 13:52:30 +0000

    extraiu palavrs ja talvez

--- script.js
+++ script.js
@@ -3,5 +3,9 @@ const BotaoMostraPalavras = document.querySelector('#botao-palavrachave');
 BotaoMostraPalavras.addEventListener('click, mostraPalavrasChave')
 
 function mostraPalavrasChave() {
-    const texto = document.querySelector('#entrada-de-texto')
+    const texto = document.querySelector('#entrada-de-texto').value;
+    const campoResultado = document.querySelector('#resultado-palavrachave');
+    const palavras = texto.split(" ");
+
+    campoResultado.textContent = palavras.join(", ");
 }
